859284f28c177e684daca3076f74c128b6812495
duxin <duxin0426@163.com>
2016-09-27 09:32:48 +0800

    修改数据库部分

--- src/sqlite/DB.java
+++ src/sqlite/DB.java
@@ -2,6 +2,13 @@ package sqlite;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.cnic.peer.entity.Piece;
 
 public class DB {
 	private static Connection conn = null;
@@ -29,5 +36,45 @@ public class DB {
 			conn = null;
 			System.gc();
 		}
+	}
+	
+	public void insertPiece(Piece p) {
+		try {
+			Connection conn = DB.openConnection();
+			String sql = "INSERT INTO ts (contentHash,offset,length) VALUES (?,?,?);"; 
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, p.getContentHash());
+			stmt.setInt(2, p.getOffset());
+			stmt.setInt(3, p.getLength());
+			stmt.executeUpdate();
+			stmt.close();
+			DB.closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Piece> getPiecesByContentHash(String contentHash) {
+		List<Piece> pieces = new ArrayList<Piece>();
+		try {
+			Connection conn = DB.openConnection();
+			String sql = "SELECT * FROM ts where contentHash = ?;";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, contentHash);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Piece p = new Piece();
+				p.setContentHash(rs.getString("contentHash"));
+				p.setOffset(rs.getInt("offset"));
+				p.setLength(rs.getInt("length"));
+				pieces.add(p);
+			}
+			rs.close();
+			stmt.close();
+			DB.closeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pieces;
 	}
 }
